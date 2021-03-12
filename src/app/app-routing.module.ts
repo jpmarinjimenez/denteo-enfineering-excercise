491bde78103bc2f4caf202ea4cd5adbdeeba1648
jpmarinjimenez <jpmarinjimenez@gmail.com>
2021-03-12 10:19:54 +0100

    HomeComponent and CalendarComponent created and linked to the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent},
+	{ path: 'calendar', component: CalendarComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'calendar-app',
+    templateUrl: './calendar.component.html',
+    styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent {
+
+}
